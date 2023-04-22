3ed59f3c1ec255574115251d34e029b3e9ebd28c
LadySerena <serena.tiede+github@gmail.com>
2023-04-22 12:50:45 -0500

    fix: use pkg-config crate
    Use pkg-config to remove hardcoded clang args. This is so ideally this
    will compile on my laptop.

--- build.rs
+++ build.rs
@@ -1,28 +1,27 @@
 extern crate bindgen;
-
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-search=/usr/include/blockdev");
-    println!("cargo:rustc-link-search=/usr/include/glib-2.0");
-    println!("cargo:rustc-link-search=/usr/lib64/glib-2.0/include");
-
-
-    println!("cargo:rustc-link-lib=blockdev");
+    let conf = pkg_config::Config::new();
+    let blockdev = conf.probe("blockdev").unwrap();
     println!("cargo:rustc-link-lib=bd_lvm");
     println!("cargo:rerun-if-changed=wrapper.h");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I/usr/include/glib-2.0")
-        .clang_arg("-I/usr/lib64/glib-2.0/include")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .clang_args(
+            blockdev
+                .include_paths
+                .iter()
+                .map(|path| format!("-I{}", path.to_string_lossy())),
+        )
         .generate()
         .expect("unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs"))
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
         .expect("couldn't write bindings");
-
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,25 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::mem;
 
     #[test]
     fn foo() {
         unsafe {
             bd_lvm_check_deps();
+        }
+    }
+
+    #[test]
+    fn list_pvs() {
+        unsafe {
+            let dep_check = bd_lvm_check_deps();
+            assert_ne!(dep_check, 0);
+            let init_check = bd_lvm_init();
+            assert_ne!(init_check, 0);
+
+            bd_lvm_pvs(std::ptr::null_mut());
+
+            bd_lvm_close();
         }
     }
 }
